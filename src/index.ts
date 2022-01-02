9d790109d988dcc93e9c51bf751756459a947a3d
donghakim <donghakim@donghakimui-MacBookPro.local>
2022-01-02 23:48:56 +0900

    feat: block class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(0, "1112002", "", "init", 12345);
